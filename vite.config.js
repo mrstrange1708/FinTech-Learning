03c5a2271bf8ecf07a828e1054a94ca102427a2c
mrstrange1708 <junaidsamishaik@gmail.com>
2025-04-09 21:40:57 +0530

    Added 404.html for router fallback on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/FinTech-Learning/',   // 👈 Add this line
+  base: '/FinTech-Learning/',
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,   // 👈 Important for React Router
+  }
 })
