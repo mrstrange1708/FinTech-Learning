7d340983486383229dd7b34373ef0eba88c286e4
mrstrange1708 <junaidsamishaik@gmail.com>
2025-04-09 21:50:23 +0530

    Trying to Host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FinTech-Learning/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react()
